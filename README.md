0af042bf9f3198d5e87893dd9cee294953adfb1c
fanyinghao <fanyinghao@gmail.com>
2020-01-04 10:39:56 +0800

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# nasgo-adapter
+
+nasgo-adapter适配了openwallet.AssetsAdapter接口，给应用提供了底层的区块链协议支持。
+
+## 如何测试
+
+openwtester包下的测试用例已经集成了openwallet钱包体系，创建conf目录，新建NSG.ini文件，编辑如下内容：
+
+```ini
+
+# node api url
+serverAPI = "http://127.0.0.1:1005"
+
+```
+
+## 资料介绍
+
+### 官网
+
+https://www.nasgo.com/
+
+### 区块浏览器
+
+http://explorer.nasgo.com/
+
+### github
+https://github.com/NASGO/nasgo
+
+### 适配资料
---
+++ nasgo/config.go
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2018 The openwallet Authors
+ * This file is part of the openwallet library.
+ *
+ * The openwallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The openwallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package nasgo
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/blocktree/go-owcrypt"
+	"github.com/blocktree/openwallet/common/file"
+)
+
+const (
+	//币种
+	Symbol    = "NSG"
+	CurveType = owcrypt.ECC_CURVE_ED25519
+	Decimals  = int32(8)
+	//默认配置内容
+	defaultConfig = `
+
+# RPC api url
+ServerAPI = ""
+FixFees=0.001
+`
+)
+
+type WalletConfig struct {
+
+	//币种
+	Symbol string
+	//配置文件路径
+	configFilePath string
+	//配置文件名
+	configFileName string
+	//区块链数据文件
+	BlockchainFile string
+	//本地数据库文件路径
+	dbPath string
+	//钱包服务API
+	ServerAPI string
+	//默认配置内容
+	DefaultConfig string
+	//曲线类型
+	CurveType uint32
+	//是否测试网
+	IsTestNet bool
+	//最大的输入数量
+	MaxTxInputs int
+	//数据目录
+	DataDir string
+	//固定手续费
+	FixFees string
+}
+
+func NewConfig(symbol string) *WalletConfig {
+
+	c := WalletConfig{}
+
+	//币种
+	c.Symbol = symbol
+	c.CurveType = CurveType
+	//配置文件路径
+	c.configFilePath = filepath.Join("conf")
+	//配置文件名
+	c.configFileName = c.Symbol + ".ini"
+	//区块链数据文件
+	c.BlockchainFile = "blockchain.db"
+	//本地数据库文件路径
+	c.dbPath = filepath.Join("data", strings.ToLower(c.Symbol), "db")
+	//钱包服务API
+	c.ServerAPI = ""
+	//最大的输入数量
+	c.MaxTxInputs = 50
+	c.FixFees = "0"
+
+	//创建目录
+	//file.MkdirAll(c.dbPath)
+
+	return &c
+}
+
+//创建文件夹
+func (wc *WalletConfig) makeDataDir() {
+
+	if len(wc.DataDir) == 0 {
+		//默认路径当前文件夹./data
+		wc.DataDir = "data"
+	}
+
+	//本地数据库文件路径
+	wc.dbPath = filepath.Join(wc.DataDir, strings.ToLower(wc.Symbol), "db")
+
+	//创建目录
+	file.MkdirAll(wc.dbPath)
+}
